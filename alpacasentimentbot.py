cf923a23d55f7174b47e9bcb7c882404c0f02a24
todd <toddwebb27@gmail.com>
2022-09-11 19:55:56 -0400

    Fixed Stream Loop

--- alpacasentimentbot.py
+++ alpacasentimentbot.py
@@ -5,6 +5,10 @@ API_KEY = 'PKBZFTD9KPUGALJOW7EY'
 API_SECRET = '4W8IkPJXCo7WoiEOZPVyURyc1ojnAcW299bSdXwP'
 
 classifier = pipeline('sentiment-analysis')
+stream_client = Stream(API_KEY, API_SECRET)
+rest_client = REST(API_KEY, API_SECRET)
+#historical_news = rest_client.get_news("AAPL", "2022-01-01", "2022-09-01")
+#print(historical_news)
 
 
 async def news_data_handler(news):
@@ -16,12 +20,11 @@ async def news_data_handler(news):
 	sentiment = classifier(relevant_text)
 
 	if sentiment['label'] == 'POSITIVE' and sentiment['score'] > 0.95:
-		rest_client.submit_order("AAVEUSD", 100)
+		rest_client.submit_order("AAPL", 100)
 
 	elif sentiment['label'] == 'NEGATIVE' and sentiment['score'] > 0.95:
-		rest_client.submit_order("AAVEUSD", -100)
+		rest_client.submit_order("AAPL", -100)
 
 
-	stream_client.subscribe_news(news_data_handler, "AAVEUSD")
-
-	stream_client.run()
+stream_client.subscribe_news(news_data_handler, "AAPL")
+stream_client.run()
