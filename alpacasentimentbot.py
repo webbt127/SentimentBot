b4b0e7a216c0781cd3e0f52fd40ed1dbf22cbe99
todd <toddwebb27@gmail.com>
2022-09-12 00:03:07 -0400

    Added more print statements to see what is being traded. Added extra try/except statements to catch possible failed orders. Added more detailed order args including notional buy quantities.

--- alpacasentimentbot.py
+++ alpacasentimentbot.py
@@ -23,8 +23,14 @@ async def news_data_handler(news):
 	headline = news.headline
 	tickers = news.symbols
 
+	print("News Event for", tickers, "!")
+
 	relevant_text = summary + headline
 	sentiment = classifier(relevant_text)
+
+	print(relevant_text)
+	print("Sentiment:", sentiment['label'])
+	print("Score:", sentiment['score'])
 
 	api = tradeapi.REST(API_KEY, API_SECRET, endpoint)
 	clock = api.get_clock()
@@ -32,13 +38,25 @@ async def news_data_handler(news):
 	for ticker in tickers:
 		try:
 			position = api.get_position(ticker)
-			print(position)
 			if sentiment['label'] == 'NEGATIVE' and sentiment['score'] > 0.95:
-				rest_client.submit_order(ticker, position.qty)
+				try:
+					rest_client.submit_order(symbol=ticker, qty=position.qty, side='sell', type='market', time_in_force='gtc')
+					print(ticker, "Market Sell Order Submitted!")
+				except Exception as e:
+					print(ticker, "Market Sell Order Failed!", e)
+			else:
+				print(ticker, "Positive Sentiment Found But Position Already Exists!")
 		except Exception as e:
 			if sentiment['label'] == 'POSITIVE' and sentiment['score'] > 0.95:
-				rest_client.submit_order(ticker, 1)
+				try:
+					rest_client.submit_order(symbol=ticker, notional=1000, side='buy', type='market', time_in_force='gtc')
+					print(ticker, "Market Buy Order Submitted!")
+				except Exception as e:
+					print(ticker, "Market Buy Order Failed!", e)
+			else:
+				print(ticker, "Negative Sentiment Found But No Existing Position!")
 
 
 stream_client.subscribe_news(news_data_handler, "*")
+print("Stream Client Starting, Waiting For Market News...")
 stream_client.run()
