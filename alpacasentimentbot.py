0b06ed746c79d7c1e336f3e86762b1c1287b5149
todd <toddwebb27@gmail.com>
2022-09-11 23:14:15 -0400

    Fixed Alpaca API call error.

--- alpacasentimentbot.py
+++ alpacasentimentbot.py
@@ -1,6 +1,6 @@
 from alpaca_trade_api import REST, Stream
 from transformers import pipeline, BertTokenizer, BertForSequenceClassification
-import alpaca_trade_api as api
+import alpaca_trade_api as tradeapi
 
 API_KEY = 'PKBZFTD9KPUGALJOW7EY'
 API_SECRET = '4W8IkPJXCo7WoiEOZPVyURyc1ojnAcW299bSdXwP'
@@ -12,6 +12,7 @@ tokenizer = BertTokenizer.from_pretrained("ahmedrachid/FinancialBERT-Sentiment-A
 classifier = pipeline('sentiment-analysis', model=model, tokenizer=tokenizer)
 stream_client = Stream(API_KEY, API_SECRET)
 rest_client = REST(API_KEY, API_SECRET)
+endpoint = "https://paper-api.alpaca.markets"
 #historical_news = rest_client.get_news("*", "2022-08-29", "2022-09-01")
 #print(historical_news)
 
@@ -25,6 +26,7 @@ async def news_data_handler(news):
 	relevant_text = summary + headline
 	sentiment = classifier(relevant_text)
 
+	api = tradeapi.REST(API_KEY, API_SECRET, endpoint)
 	clock = api.get_clock()
 
 	for ticker in tickers:
