cb5ef5754623f4521f88e3ea59b8fcbcb32cde20
webbt127 <80007652+webbt127@users.noreply.github.com>
2022-09-13 08:28:17 -0500

    Update alpacasentimentbot.py
    Removed neutral statements from buy/sell loop.

--- alpacasentimentbot.py
+++ alpacasentimentbot.py
@@ -36,31 +36,32 @@ async def news_data_handler(news):
 	api = tradeapi.REST(API_KEY, API_SECRET, endpoint)
 	clock = api.get_clock()
 
-	for ticker in tickers:
-		stock_info = yf.Ticker(ticker).info
-		stock_price = stock_info['regularMarketPrice']
-		buy_shares = round(1000/stock_price)
-		try:
-			position = api.get_position(ticker)
-			print("Selling", ticker,"...")
-			if sentiment[0]['label'] == 'negative' and sentiment[0]['score'] > 0.95:
-				try:
-					rest_client.submit_order(symbol=ticker, qty=position.qty, side='sell', type='market', time_in_force='gtc')
-					print("Market Sell Order Submitted!")
-				except Exception as e:
-					print("Market Sell Order Failed!", e)
-			else:
-				print("Conditions not sufficient to sell.")
-		except Exception as e:
-			print("Buying", ticker,"...")
-			if sentiment[0]['label'] == 'positive' and sentiment[0]['score'] > 0.95:
-				try:
-					rest_client.submit_order(symbol=ticker, qty=buy_shares, side='buy', type='market', time_in_force='gtc')
-					print("Market Buy Order Submitted!")
-				except Exception as e:
-					print("Market Buy Order Failed!", e)
-			else:
-				print("Conditions not sufficient to buy.")
+	if sentiment[0]['label'] != 'neutral'
+		for ticker in tickers:
+			stock_info = yf.Ticker(ticker).info
+			stock_price = stock_info['regularMarketPrice']
+			buy_shares = round(1000/stock_price)
+			try:
+				position = api.get_position(ticker)
+				print("Selling", ticker,"...")
+				if sentiment[0]['label'] == 'negative' and sentiment[0]['score'] > 0.95:
+					try:
+						rest_client.submit_order(symbol=ticker, qty=position.qty, side='sell', type='market', time_in_force='gtc')
+						print("Market Sell Order Submitted!")
+					except Exception as e:
+						print("Market Sell Order Failed!", e)
+				else:
+					print("Conditions not sufficient to sell.")
+			except Exception as e:
+				print("Buying", ticker,"...")
+				if sentiment[0]['label'] == 'positive' and sentiment[0]['score'] > 0.95:
+					try:
+						rest_client.submit_order(symbol=ticker, qty=buy_shares, side='buy', type='market', time_in_force='gtc')
+						print("Market Buy Order Submitted!")
+					except Exception as e:
+						print("Market Buy Order Failed!", e)
+				else:
+					print("Conditions not sufficient to buy.")
 
 
 stream_client.subscribe_news(news_data_handler, "*")
