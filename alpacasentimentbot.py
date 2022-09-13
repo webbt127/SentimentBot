9223975852c7bfe4d23e017e6e1a939b30e0aa91
todd <toddwebb27@gmail.com>
2022-09-12 20:33:31 -0400

    Fixed list index inside transformer output.

--- alpacasentimentbot.py
+++ alpacasentimentbot.py
@@ -29,8 +29,8 @@ async def news_data_handler(news):
 	sentiment = classifier(relevant_text)
 
 	print(relevant_text)
-	print("Sentiment:", sentiment['label'])
-	print("Score:", sentiment['score'])
+	print("Sentiment:", sentiment[0]['label'])
+	print("Score:", sentiment[0]['score'])
 
 	api = tradeapi.REST(API_KEY, API_SECRET, endpoint)
 	clock = api.get_clock()
@@ -38,23 +38,23 @@ async def news_data_handler(news):
 	for ticker in tickers:
 		try:
 			position = api.get_position(ticker)
-			if sentiment['label'] == 'NEGATIVE' and sentiment['score'] > 0.95:
+			if sentiment[0]['label'] == 'NEGATIVE' and sentiment[0]['score'] > 0.95:
 				try:
 					rest_client.submit_order(symbol=ticker, qty=position.qty, side='sell', type='market', time_in_force='gtc')
 					print(ticker, "Market Sell Order Submitted!")
 				except Exception as e:
 					print(ticker, "Market Sell Order Failed!", e)
 			else:
-				print(ticker, "Positive Sentiment Found But Position Already Exists!")
+				print(ticker, "Position Already Exists! Condition not sufficient to sell.")
 		except Exception as e:
-			if sentiment['label'] == 'POSITIVE' and sentiment['score'] > 0.95:
+			if sentiment[0]['label'] == 'POSITIVE' and sentiment[0]['score'] > 0.95:
 				try:
 					rest_client.submit_order(symbol=ticker, notional=1000, side='buy', type='market', time_in_force='gtc')
 					print(ticker, "Market Buy Order Submitted!")
 				except Exception as e:
 					print(ticker, "Market Buy Order Failed!", e)
 			else:
-				print(ticker, "Negative Sentiment Found But No Existing Position!")
+				print(ticker, "No Existing Position! Condition not sufficient to buy")
 
 
 stream_client.subscribe_news(news_data_handler, "*")
