f168418e9ed46c9cc96a9bff2eaab43d91bcccc5
webbt127 <80007652+webbt127@users.noreply.github.com>
2022-09-14 18:49:15 -0500

    Update alpacasentimentbot.py
    Changed to market orders.
    Testing clock.

--- alpacasentimentbot.py
+++ alpacasentimentbot.py
@@ -2,6 +2,7 @@ from alpaca_trade_api import REST, Stream
 from transformers import pipeline, BertTokenizer, BertForSequenceClassification
 import alpaca_trade_api as tradeapi
 import yfinance as yf
+import datetime
 
 API_KEY = 'PKVLATR3V44A75JDC07W'
 API_SECRET = 'ngvSepjmymi5Aj0wn13kU6h1ZJyfzfMkQxfDzvmv'
@@ -43,18 +44,16 @@ async def news_data_handler(news):
 
 	api = tradeapi.REST(API_KEY, API_SECRET, endpoint)
 	clock = api.get_clock()
+	print(datetime(clock.next_close)
 
 	if sentiment[0]['label'] != 'neutral' and sentiment[0]['score'] > 0.95 and news.id != previous_id:
 		for ticker in tickers:
 			try:
 				position = api.get_position(ticker)
 				print("Selling", ticker,"...")
-				if sentiment[0]['label'] == 'negative' and sentiment[0]['score'] > 0.95:
+				if sentiment[0]['label'] == 'negative' and sentiment[0]['score'] > 0.95 and clock.is_open:
 					try:
-						stock_info = yf.Ticker(ticker).info
-						stock_price = stock_info['regularMarketPrice']
-						stock_price_order = round(stock_price * 0.98)
-						rest_client.submit_order(symbol=ticker, qty=position.qty, side='sell', type='limit', limit_price=stock_price_order, time_in_force='gtc')
+						rest_client.submit_order(symbol=ticker, qty=position.qty, side='sell', type='market', time_in_force='gtc')
 						print("Market Sell Order Submitted!")
 					except Exception as e:
 						print("Market Sell Order Failed!", e)
@@ -62,20 +61,19 @@ async def news_data_handler(news):
 					print("Conditions not sufficient to sell.")
 			except Exception as e:
 				print("Buying", ticker,"...")
-				if sentiment[0]['label'] == 'positive' and sentiment[0]['score'] > 0.95:
+				if sentiment[0]['label'] == 'positive' and sentiment[0]['score'] > 0.95 and clock.is_open:
 					try:
 						stock_info = yf.Ticker(ticker).info
 						stock_price = stock_info['regularMarketPrice']
 						buy_shares = round(1000/stock_price)
-						stock_price_order = round(stock_price *1.02)
-						rest_client.submit_order(symbol=ticker, qty=buy_shares, side='buy', type='limit', limit_price=stock_price_order, time_in_force='gtc')
+						rest_client.submit_order(symbol=ticker, qty=buy_shares, side='buy', type='market', time_in_force='gtc')
 						print("Market Buy Order Submitted!")
 					except Exception as e:
 						print("Market Buy Order Failed!", e)
 				else:
 					print("Conditions not sufficient to buy.")
+		print("Waiting For Market News...")
 	previous_id = news.id
-	print("Waiting For Market News...")
 
 
 stream_client.subscribe_news(news_data_handler, "*")
