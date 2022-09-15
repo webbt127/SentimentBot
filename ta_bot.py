6d20b86185e78d47de38e117a8db8b30c36b54cf
webbt127 <80007652+webbt127@users.noreply.github.com>
2022-09-15 09:37:44 -0500

    Update ta_bot.py

--- ta_bot.py
+++ ta_bot.py
@@ -10,33 +10,35 @@ endpoint = "https://paper-api.alpaca.markets"
 rest_client = REST(API_KEY, API_SECRET, endpoint)
 api = tradeapi.REST(API_KEY, API_SECRET, endpoint)
 clock = api.get_clock()
-position_list = api.list_positions()
-position_list_size = len(position_list)
-positions = range(0, position_list_size - 1)
+
 while True:
-  while position_list_size > 0 and clock.is_open:
-    for position in positions:
-      ticker = position_list[position].__getattr__('symbol')
-      exchange = position_list[position].__getattr__('exchange')
-      position_size = api.get_position(ticker)
-      position_t = api.get_position(ticker)
-      print(position_t.exchange)
-      print("Open Positions:", ticker)
-      try:
-        ticker_ta = TA_Handler(symbol=ticker, screener="america", exchange=exchange, interval=Interval.INTERVAL_1_HOUR)
-        summary = ticker_ta.get_analysis().summary
-      except Exception as e:
-        print("No TA Data Available for", ticker)
-      recommendation = summary['RECOMMENDATION']
-      print(recommendation)
-      if recommendation == 'SELL' or recommendation == 'STRONG_SELL':
+  while clock.is_open:
+    position_list = api.list_positions()
+    position_list_size = len(position_list)
+    positions = range(0, position_list_size - 1)
+    while positions > 0:
+      for position in positions:
+        ticker = position_list[position].__getattr__('symbol')
+        exchange = position_list[position].__getattr__('exchange')
+        position_size = api.get_position(ticker)
+        position_t = api.get_position(ticker)
+        print(position_t.exchange)
+        print("Open Positions:", ticker)
         try:
-          rest_client.submit_order(symbol=ticker, qty=position_size.qty, side='sell', type='market', time_in_force='gtc')
-          print("Market Sell Order Submitted!")
+          ticker_ta = TA_Handler(symbol=ticker, screener="america", exchange=exchange, interval=Interval.INTERVAL_1_HOUR)
+          summary = ticker_ta.get_analysis().summary
         except Exception as e:
-          print("Market Sell Order Failed!", e)
-  print("No Open Positions Or Market is Closed, Sleeping 10 minutes...")
+          print("No TA Data Available for", ticker)
+        recommendation = summary['RECOMMENDATION']
+        print(recommendation)
+        if recommendation == 'SELL' or recommendation == 'STRONG_SELL':
+          try:
+            rest_client.submit_order(symbol=ticker, qty=position_size.qty, side='sell', type='market', time_in_force='gtc')
+            print("Market Sell Order Submitted!")
+          except Exception as e:
+            print("Market Sell Order Failed!", e)
+    print("No Open Positions Or Market is Closed, Sleeping 10 minutes...")
   if clock.is_open == 0:
     api.cancel_all_orders()
     print("All Open Orders Cancelled!")
-  time.sleep(600)
+    time.sleep(600)
