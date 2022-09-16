691e8dfe8196b35e7f69094d671e638807fd7b86
webbt127 <80007652+webbt127@users.noreply.github.com>
2022-09-15 19:40:01 -0500

    Update alpacasentimentbot.py

--- alpacasentimentbot.py
+++ alpacasentimentbot.py
@@ -4,26 +4,37 @@ import alpaca_trade_api as tradeapi
 import yfinance as yf
 from datetime import datetime, timezone
 from tradingview_ta import TA_Handler, Interval, Exchange
+from logger import *
+import gvars
 
-API_KEY = 'PKGHWK2YIALYGDM0L76G'
-API_SECRET = 'afiiLs9sSFnJqyTcwIuRY71IZyyqfyYzup9t3GPh'
-endpoint = "https://paper-api.alpaca.markets"
+initialize_logger()
 
-print("Loading Machine Learning Model...")
+lg.info("Loading Machine Learning Model...")
 model = BertForSequenceClassification.from_pretrained("ahmedrachid/FinancialBERT-Sentiment-Analysis",num_labels=3)
 tokenizer = BertTokenizer.from_pretrained("ahmedrachid/FinancialBERT-Sentiment-Analysis")
-print("Machine Learning Model Loaded!")
+lg.info("Machine Learning Model Loaded!")
 
-print("Loading Classifier...")
-#classifier = pipeline('sentiment-analysis')
+lg.info("Loading Classifier...")
 classifier = pipeline('sentiment-analysis', model=model, tokenizer=tokenizer)
-stream_client = Stream(API_KEY, API_SECRET)
-rest_client = REST(API_KEY, API_SECRET, endpoint)
-print("Classifier Loaded!")
-#historical_news = rest_client.get_news("*", "2022-08-29", "2022-09-01")
-#print(historical_news)
+stream_client = Stream(gvars.API_KEY, gvars.API_SECRET_KEY)
+rest_client = REST(gvars.API_KEY, gvars.API_SECRET_KEY, gvars.API_URL)
+lg.info("Classifier Loaded!")
 previous_id = 0
 
+def check_ta(ticker, exchange):
+	try:
+		ticker_ta = TA_Handler(symbol=ticker, screener="america", exchange=exchange, interval=Interval.INTERVAL_1_HOUR)
+	except Exception as e:
+		lg.info("Unable To Find Ticker TA!", e)
+	summary = ticker_ta.get_analysis().summary
+	recommendation = summary['RECOMMENDATION']
+	return recommendation
+
+def get_exchange(ticker):
+	assets = api.list_assets()
+	print(assets[0].symbol)
+	if ticker == assets[0].symbol:
+		return assets[0].exchange
 
 async def news_data_handler(news):
 
@@ -35,13 +46,13 @@ async def news_data_handler(news):
 	relevant_text = summary + headline
 	sentiment = classifier(relevant_text)
 	if previous_id != news.id:
-		print("News Event for", tickers)
-		print(relevant_text)
-		print("Sentiment:", sentiment[0]['label'])
-		print("Score:", sentiment[0]['score'])
-		print("ID:", news.id)
+		lg.info("News Event for", tickers)
+		lg.info(relevant_text)
+		lg.info("Sentiment:", sentiment[0]['label'])
+		lg.info("Score:", sentiment[0]['score'])
+		lg.info("ID:", news.id)
 	else:
-		print("Duplicate ID, skipping...")
+		lg.info("Duplicate ID, skipping...")
 
 	api = tradeapi.REST(API_KEY, API_SECRET, endpoint)
 	clock = api.get_clock()
@@ -51,40 +62,35 @@ async def news_data_handler(news):
 
 	if news.id != previous_id:
 		for ticker in tickers:
-			try:
-				ticker_ta = TA_Handler(symbol=ticker, screener="america", exchange='nyse', interval=Interval.INTERVAL_1_HOUR)
-			except Exception as e:
-				ticker_ta = TA_Handler(symbol=ticker, screener="america", exchange='nasdaq', interval=Interval.INTERVAL_1_HOUR)
-			summary = ticker_ta.get_analysis().summary
-			recommendation = summary['RECOMMENDATION']
-			try:
-				position = api.get_position(ticker)
-				print("Selling", ticker,"...")
+			exchange = get_exchange(ticker)
+			ta = check_ta(ticker, exchange)
+			position = api.get_position(ticker)
+			lg.info("Selling", ticker,"...")
 				if (((sentiment[0]['label'] == 'negative' and sentiment[0]['score'] > 0.95) or recommendation == 'SELL' or recommendation == 'STRONG_SELL') and clock.is_open):
 					try:
 						rest_client.submit_order(symbol=ticker, qty=position.qty, side='sell', type='market', time_in_force='gtc')
-						print("Market Sell Order Submitted!")
+						lg.info("Market Sell Order Submitted!")
 					except Exception as e:
-						print("Market Sell Order Failed!", e)
+						lg.info("Market Sell Order Failed!", e)
 				else:
-					print("Conditions not sufficient to sell.")
+					lg.info("Conditions not sufficient to sell.")
 			except Exception as e:
-				print("Buying", ticker,"...")
+				lg.info("Buying", ticker,"...")
 				if sentiment[0]['label'] == 'positive' and sentiment[0]['score'] > 0.95 and (recommendation == 'BUY' or recommendation == 'STRONG_BUY') and clock.is_open:
 					try:
 						stock_info = yf.Ticker(ticker).info
 						stock_price = stock_info['regularMarketPrice']
 						buy_shares = round(1000/stock_price)
 						rest_client.submit_order(symbol=ticker, qty=buy_shares, side='buy', type='market', time_in_force='gtc')
-						print("Market Buy Order Submitted!")
+						lg.info("Market Buy Order Submitted!")
 					except Exception as e:
-						print("Market Buy Order Failed!", e)
+						lg.info("Market Buy Order Failed!", e)
 				else:
-					print("Conditions not sufficient to buy.")
-		print("Waiting For Market News...")
+					lg.info("Conditions not sufficient to buy.")
+		lg.info("Waiting For Market News...")
 	previous_id = news.id
 
 
 stream_client.subscribe_news(news_data_handler, "*")
-print("Stream Client Starting, Waiting For Market News...")
+lg.info("Stream Client Starting, Waiting For Market News...")
 stream_client.run()
