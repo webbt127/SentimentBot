da3ef2f0e6ed2e78d3bee58c8b46bba883a48281
webbt127 <80007652+webbt127@users.noreply.github.com>
2022-09-16 09:11:25 -0500

    Update alpacasentimentbot.py

--- alpacasentimentbot.py
+++ alpacasentimentbot.py
@@ -96,6 +96,7 @@ def client_thread2():
 				exchange = position_list[position].__getattr__('exchange')
 				position_size = rest_client.get_position(ticker)
 				qty = position_size.qty * -1
+				print(qty)
 				ta = check_ta(ticker, exchange)
 				lg.info(ticker)
 				lg.info(ta)
