5b8fc489e51c9953b4e0854171216f5fb6ee2c70
webbt127 <80007652+webbt127@users.noreply.github.com>
2022-09-20 23:11:50 -0500

    Update alpacasentimentbot.py

--- alpacasentimentbot.py
+++ alpacasentimentbot.py
@@ -126,8 +126,11 @@ def begin_threading():
 	thread1 = threading.Thread(target=news_thread)
 	thread2 = threading.Thread(target=analysis_thread)
 	thread1.start()
+	time.sleep(5)
 	thread2.start()
+	time.sleep(5)
 	thread1.join()
+	time.sleep(5)
 	thread2.join()
 	
 def get_positions():
@@ -195,11 +198,10 @@ def analysis_thread():
 			market_open = check_market_availability()
 			positions, position_list_size, position_list = get_positions()
 		lg.info("No Open Positions Or Market is Closed, Sleeping 10 minutes...")
-		for total in 600000:
-			with alive_bar(total) as bar:
-				for _ in range(600000):
-					time.sleep(.001)
-					bar()
+		with alive_bar(gvars.market_sleep_time) as bar:
+			for _ in range(gvars.market_sleep_time):
+				time.sleep(1)
+				bar()
 	
 
 ###################INITIALIZATIONS AND RUN MAIN LOOP###################	
