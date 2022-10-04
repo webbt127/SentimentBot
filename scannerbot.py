b1a370c67bb3da13a9f18a4cd38f007b977ed228
webbt127 <80007652+webbt127@users.noreply.github.com>
2022-10-04 16:56:25 -0500

    Update scannerbot.py

--- scannerbot.py
+++ scannerbot.py
@@ -10,7 +10,7 @@ import gvars
 from joblib import Parallel, delayed, parallel_backend
 import time
 from alive_progress import alive_bar
-from tqdm.auto import tqdm
+from tqdm.notebook import tqdm
 
 
 def check_ta(ticker, exchange):
