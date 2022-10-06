3457fb833f10f770887e30a6e7ee64fcb1d211a1
webbt127 <80007652+webbt127@users.noreply.github.com>
2022-10-06 15:46:28 -0500

    Update gvars.py

--- gvars.py
+++ gvars.py
@@ -10,6 +10,6 @@ reddit_buy_threshold = 60
 market_sleep_time = 6000
 loop_sleep_time = 60
 min_sentiment_score = 0.99
-order_size_usd = 1000
+order_size_usd = 250
 minutes_min = 660
 minutes_max = 1140
