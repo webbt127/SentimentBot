87294f2e20c92d2be49fa58049cdcd7d7d129c49
webbt127 <80007652+webbt127@users.noreply.github.com>
2022-10-06 16:37:30 -0500

    Update scannerbot.py

--- scannerbot.py
+++ scannerbot.py
@@ -275,10 +275,10 @@ def analysis_thread():
                                      
 			lg.info("PCR Count Above 2.0: %s" % pcr_count)	
 			assets = [a for a in active_assets if a.exchange == 'NASDAQ']
-			with alive_bar(len(active_assets)) as bar():
+			with alive_bar(len(active_assets)) as buy_bar():
 				for i in active_assets:
 					run_buy_loop(i)
-					bar()
+					buy_bar()
 			market_open = check_market_availability()
 			positions, position_list_size, position_list = get_positions()
 		lg.info("Market is Closed, Sleeping...")
