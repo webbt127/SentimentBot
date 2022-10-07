e5d707595199c4189988d80411c35415f71d9994
webbt127 <80007652+webbt127@users.noreply.github.com>
2022-10-07 08:54:22 -0500

    Update scannerbot.py

--- scannerbot.py
+++ scannerbot.py
@@ -225,34 +225,33 @@ def no_operation():
 def run_buy_loop(asset):
 	ticker = asset.symbol
 	exchange = asset.exchange
-	if exchange == 'NASDAQ' or exchange == 'NYSE':
-		ta = check_ta(ticker, exchange)
-		if ta == 'STRONG_BUY':
-			current_position = get_ticker_position(ticker)
-			if current_position == 0:
-				pcr = get_pcr(ticker)
-				if pcr > 2.0:
-					stock_price = get_price(ticker)
-					if stock_price is not None:
-						new_qty = round(gvars.order_size_usd/(stock_price + .0000000000001))
-					else:
-						new_qty = 0
-					pivots = get_pivots(ticker, exchange, stock_price)
-					print(pivots)
-					new_qty = new_qty * pivots
-					if pivots > 0:
-						submit_buy_order(ticker, new_qty, ta, pcr)
-					else:
-						no_operation()
+	ta = check_ta(ticker, exchange)
+	if ta == 'STRONG_BUY':
+		current_position = get_ticker_position(ticker)
+		if current_position == 0:
+			pcr = get_pcr(ticker)
+			if pcr > 2.0:
+				stock_price = get_price(ticker)
+				if stock_price is not None:
+					new_qty = round(gvars.order_size_usd/(stock_price + .0000000000001))
+				else:
+					new_qty = 0
+				pivots = get_pivots(ticker, exchange, stock_price)
+				print(pivots)
+				new_qty = new_qty * pivots
+				if pivots > 0:
+					submit_buy_order(ticker, new_qty, ta, pcr)
 				else:
 					no_operation()
-					#lg.info("PCR not sufficient to buy %s." % ticker)
 			else:
 				no_operation()
-				#lg.info("Position Already Exists!")
+				#lg.info("PCR not sufficient to buy %s." % ticker)
 		else:
 			no_operation()
-			#lg.info("TA Not Sufficient For %s!" % ticker)
+			#lg.info("Position Already Exists!")
+	else:
+		no_operation()
+		#lg.info("TA Not Sufficient For %s!" % ticker)
 	
 def analysis_thread():
 	while 1:
@@ -275,7 +274,7 @@ def analysis_thread():
                                      
 			lg.info("PCR Count Above 2.0: %s" % pcr_count)	
 			assets = [a for a in active_assets if a.exchange == 'NASDAQ']
-			with alive_bar(len(assets)) as bar:
+			with alive_bar(len(active_assets)) as bar:
 				for i in active_assets:
 					run_buy_loop(i)
 					bar()
@@ -292,7 +291,6 @@ initialize_logger()
 rest_client = REST(gvars.API_KEY, gvars.API_SECRET_KEY, gvars.API_URL)
 
 market_open = check_market_availability() # initial time check
-#get_pivots('TXN', 'NASDAQ')
 positions = get_positions() # check existing positions before iterating
 assets = rest_client.list_assets(status='active')
 active_assets = [a for a in assets if a.exchange == 'NASDAQ' or a.exchange == 'NYSE']
