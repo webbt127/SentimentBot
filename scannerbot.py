9505ee3914b9274c25dabaaf306f4226bdcf79da
webbt127 <80007652+webbt127@users.noreply.github.com>
2022-10-08 23:26:40 -0500

    Update scannerbot.py

--- scannerbot.py
+++ scannerbot.py
@@ -157,5 +157,6 @@ market_open = check_market_availability() # initial time check
 positions = get_positions() # check existing positions before iterating
 active_assets = rest_client.list_assets(status='active')
 assets = [a for a in active_assets if (a.exchange == 'NASDAQ' or a.exchange == 'NYSE') and a.tradable == True]
+print(assets)
 cancel_orders() # cancel all open orders before iterating
 main_loop(assets)
