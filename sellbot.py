e010ff48923209e16b75ad5c5bed60ff85aa264c
webbt127 <80007652+webbt127@users.noreply.github.com>
2022-10-10 10:27:47 -0500

    Update sellbot.py

--- sellbot.py
+++ sellbot.py
@@ -114,9 +114,8 @@ def run_sell_loop(positions):
 	for position in positions:
 		get_ticker_position(position)
 		check_ta(position)
-		print(position)
-		get_price(position)
 		get_pivots(position)
+		position.price = position.current_price
 		#pcr = get_pcr(position.symbol)
 		if position.ta == 'STRONG_SELL' or position.ta == 'SELL' or position.pivot < 0:#pcr < 1.0:
 			submit_sell_order(position)
@@ -157,5 +156,6 @@ market_open = check_market_availability() # initial time check
 positions = get_positions() # check existing positions before iterating
 active_assets = rest_client.list_assets(status='active')
 assets = [a for a in active_assets if (a.exchange == 'NASDAQ' or a.exchange == 'NYSE') and a.tradable == True]
+print(assets)
 cancel_orders() # cancel all open orders before iterating
 main_loop(assets)
