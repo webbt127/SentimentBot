7b9c12bea1938cc5964fa596777d0a179a1decf5
webbt127 <80007652+webbt127@users.noreply.github.com>
2022-10-10 12:12:36 -0500

    Update helper_functions.py

--- helper_functions.py
+++ helper_functions.py
@@ -148,9 +148,9 @@ def get_all_prices(assets):
 	for asset in assets:
 		asset_list.append(asset.symbol)
 	try:
-		stock_info = yf.Tickers(asset_list)
+		tickers = yf.Tickers(asset_list)
 	except Exception as e:
 		lg.info(e)
 	for asset in assets:
-		asset.price = stock_info[asset.symbol].info['regularMarketPrice']
+		asset.price = tickers.tickers[asset.symbol].info['regularMarketPrice']
 		print(asset)
